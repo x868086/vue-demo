57efaf37424ef8f241933176446d4488c2882abd
x868086 <43070810@qq.com>
2018-07-05 08:47:42 +0800

    webpack_reload

--- vuedemo/build/webpack.dev.conf.js
+++ vuedemo/build/webpack.dev.conf.js
@@ -22,27 +22,30 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 
   // these devServer options should be customized in /config/index.js
   devServer: {
-    clientLogLevel: 'warning',
-    historyApiFallback: {
-      rewrites: [
-        { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html') },
-      ],
-    },
-    hot: true,
-    contentBase: false, // since we use CopyWebpackPlugin.
-    compress: true,
-    host: HOST || config.dev.host,
-    port: PORT || config.dev.port,
-    open: config.dev.autoOpenBrowser,
-    overlay: config.dev.errorOverlay
-      ? { warnings: false, errors: true }
-      : false,
-    publicPath: config.dev.assetsPublicPath,
-    proxy: config.dev.proxyTable,
-    quiet: true, // necessary for FriendlyErrorsPlugin
-    watchOptions: {
-      poll: config.dev.poll,
-    }
+    // clientLogLevel: 'warning',
+    // historyApiFallback: {
+    //   rewrites: [
+    //     { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html') },
+    //   ],
+    // },
+    // hot: true,
+    // contentBase: false, // since we use CopyWebpackPlugin.
+    // compress: true,
+    // host: HOST || config.dev.host,
+    // port: PORT || config.dev.port,
+    // open: config.dev.autoOpenBrowser,
+    // overlay: config.dev.errorOverlay
+    //   ? { warnings: false, errors: true }
+    //   : false,
+    // publicPath: config.dev.assetsPublicPath,
+    // proxy: config.dev.proxyTable,
+    // quiet: true, // necessary for FriendlyErrorsPlugin
+    // watchOptions: {
+    //   poll: config.dev.poll,
+    // }
+    contentBase: "../src/pages",//本地服务器所加载的页面所在的目录
+    historyApiFallback: true,//不跳转
+    inline: true//实时刷新
   },
   plugins: [
     new webpack.DefinePlugin({
